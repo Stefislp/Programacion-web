0d09dbbf54fd834a0a93b87bbb0e3098e225fa15
Stefania Saint Lary <123702207+Stefislp@users.noreply.github.com>
2023-03-24 02:38:17 -0300

    Update top.js

--- top.js
+++ top.js
@@ -1,5 +1,12 @@
 const mybutton = document.querySelector("#btn_scrolltop");
 
+let lineaProgreso = document.querySelectorAll(".linea-progreso"),
+valorProgreso = document.querySelectorAll(".valor-progreso");
+
+for (let i = 0; i < lineaProgreso.length; i++) {
+  progress_function(valorProgreso[i]);
+}
+
 mybutton.addEventListener('click', topFunction);
 
 window.onscroll = function () {
@@ -17,4 +24,21 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+function progress_function(pv) {
+  let progress_star_value = 0,
+    speed = 40;
+
+  let progress_end_value = parseInt(pv.textContent);
+
+  let progress = setInterval(() => {
+    progress_star_value++;
+    pv.textContent = `${progress_star_value}%`;
+    pv.style.width = progress_star_value + "%";
+
+    if (progress_star_value === progress_end_value) {
+      clearInterval(progress);
+    }
+  }, speed);
 }
